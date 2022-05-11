bda1bfaeed74dc4f2357eb677bc9524010af1e90
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-05-12 02:10:32 +0900

    remove unused module

--- test/Admin.ts
+++ test/Admin.ts
@@ -1,4 +1,4 @@
-import { ethers, waffle } from "hardhat";
+import { ethers } from "hardhat";
 import { expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { AdminMock } from "../typechain";
