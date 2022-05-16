d5cc279404d30c5b72846c1dfb7138c7267ccd3e
Sarveshwar Singh <shin@shinlabs.net>
2022-05-16 13:22:44 +0900

    add TODOs in test

--- test/Admin.ts
+++ test/Admin.ts
@@ -20,7 +20,13 @@ describe("Admin", function () {
     cAdminContract = await cfAdminContract.deploy();
   });
 
+  describe("after deploy", function () {
+    it("TODO: check deployer is an admin");
+  });
+
   describe("batchAddAdmin", function () {
+    it("TODO: check adding zero address as admin");
+
     it("can add admin", async function () {
       await cAdminContract.batchAddAdmin([Address1.address, Address2.address]);
       expect(await cAdminContract.isAdmin(Address2.address)).to.equal(true);
@@ -37,33 +43,35 @@ describe("Admin", function () {
   });
 
   describe("addAdmin", function () {
-    it("can add admin", async function () {
-      await cAdminContract.addAdmin(Address1.address);
-      expect(await cAdminContract.isAdmin(Address1.address)).to.equal(true);
-    });
+    it("TODO: [R] adding zero address as an admin");
+
     it("[R]can not add admin from non admin signer", async function () {
       await expect(
         cAdminContract.connect(Address1).addAdmin(Address2.address)
       ).to.be.revertedWith("Admin:onlyAdmin caller is not an Admin");
     });
+
     it("[R]can not add admin zero address", async function () {
       await expect(cAdminContract.addAdmin(zeroAddress)).to.be.revertedWith(
         "Admin:addAdmin newAdmin is the zero address"
       );
     });
-  });
-  describe("removeAdmin", function () {
-    it("can remove admin", async function () {
+
+    it("can add admin", async function () {
       await cAdminContract.addAdmin(Address1.address);
       expect(await cAdminContract.isAdmin(Address1.address)).to.equal(true);
-      await cAdminContract.removeAdmin(Address1.address);
-      expect(await cAdminContract.isAdmin(Address1.address)).to.equal(false);
     });
+  });
+
+  describe("removeAdmin", function () {
+    it("TODO: [R] removing zero address");
+
     it("[R]can not remove admin from non admin signer", async function () {
       await expect(
         cAdminContract.connect(Address1).removeAdmin(Address2.address)
       ).to.be.revertedWith("Admin:onlyAdmin caller is not an Admin");
     });
+
     it("[R]can not remove admin non admin signer", async function () {
       await cAdminContract.addAdmin(Address1.address);
       await expect(
@@ -72,8 +80,18 @@ describe("Admin", function () {
         "Admin:removeAdmin trying to remove non existing Admin"
       );
     });
+
+    it("can remove admin", async function () {
+      await cAdminContract.addAdmin(Address1.address);
+      expect(await cAdminContract.isAdmin(Address1.address)).to.equal(true);
+      await cAdminContract.removeAdmin(Address1.address);
+      expect(await cAdminContract.isAdmin(Address1.address)).to.equal(false);
+    });
   });
+
   describe("isAdmin", function () {
+    it("TODO: [R] check zero address");
+
     it("check admin ", async function () {
       expect(await cAdminContract.isAdmin(ContractOwner.address)).to.equal(
         true
